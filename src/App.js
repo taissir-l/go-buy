4c0be5677cd4e2980a891ffd390277ce9ed992c2
taissir-l <taissirlassoued1@gmail.com>
2025-01-22 20:33:54 +0100

    finishing the static home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
-    <div className="app">
-      {/*Header*/}
-      <Header />
-      {/*Home*/}
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,32 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <img
-          className="home__image"
-          src="/images/banner.png"
-          alt="banner"
-        ></img>
+        <img className="home__image" src="/images/banner.png" alt="banner" />
         <div className="home__row">
-          <div className="home__rowOne">
-            <h1>Shop</h1>
-          </div>
-          <div className="home__rowTwo">
-            <h1>Shop</h1>
-          </div>
+          <Product
+            title="the watch"
+            price={26.85}
+            image="https://beautystore.tn/23657-large_default/solde-kit-multi--kit-multi-midi-glow-eponge-.jpg"
+            rating={5}
+          />
+          <Product />
+          <Product />
         </div>
-
         <div className="home__row">
-          <div className="home__rowOne">
-            <h1>Shop</h1>
-          </div>
-          <div className="home__rowTwo">
-            <h1>Shop</h1>
-          </div>
-          <div className="home__rowThree">
-            <h1>Shop</h1>
-          </div>
-          <div className="home__rowFour">
-            <h1>Shop</h1>
-          </div>
+          <Product />
+          <Product />
+          <Product />
+          <Product />
         </div>
-
         <div className="home__row">
-          <div className="home__rowOne">
-            <h1>Shop</h1>
-          </div>
-          <div className="home__rowTwo">
-            <h1>Shop</h1>
-          </div>
+          <Product />
+          <Product />
+          <Product />
         </div>
       </div>
     </div>
