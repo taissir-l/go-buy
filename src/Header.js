d2c6797f71f6aa344fee699cee7241b9320be0e7
taissir-l <taissirlassoued1@gmail.com>
2025-01-22 16:56:16 +0100

    finishing header

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 function Header() {
   return (
-    <div className='header'>
-      <img className='header__logo'></img>
+    <div className="header">
+      <img className="header__logo" src="/images/logo.jpg" alt="logo"></img>
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionOne">dfffd</span>
+          <span className="header__optionTwo">ddd</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">dfffdd</span>
+          <span className="header__optionTwo">ddd</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionOne">ddfffdd</span>
+          <span className="header__optionTwo">dddd</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
