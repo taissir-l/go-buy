31466b4badbb1dfc88a7eaa29e4b9fe5e2101c1e
taissir-l <taissirlassoued1@gmail.com>
2025-01-22 13:55:34 +0100

    creating header file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCXho4T7Yh3Ck-bF6Pj-jpdb1G02PvMjtc",
+    authDomain: "buy-and-go-efb48.firebaseapp.com",
+    projectId: "buy-and-go-efb48",
+    storageBucket: "buy-and-go-efb48.firebasestorage.app",
+    messagingSenderId: "356577730241",
+    appId: "1:356577730241:web:8f0c501e4e66dca6e93e40",
+    measurementId: "G-HZD079XY6V"
+  };
