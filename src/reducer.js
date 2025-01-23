a3110b7ff2e76214ddfc33ea49a0af5a56a6acaf
taissir-l <taissirlassoued1@gmail.com>
2025-01-23 12:20:59 +0100

    ceckout basket compleate

--- src/reducer.js
+++ src/reducer.js
@@ -2,6 +2,11 @@ export const initialState = {
   basket: [],
 };
 
+//selector
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
+//reducer
 const reducer = (state, action) => {
   console.log(action);
   switch (action.type) {
@@ -10,22 +15,22 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
-    //case "REMOVE_FROM_BASKET":
-    //const index = state.basket.findIndex(
-    // (basketItem) => basketItem.id === action.id
-    //);
-    //let newBasket = [...state.basket];
-    //if (index >= 0) {
-    //newBasket.splice(index, 1);
-    //} else {
-    // console.warn(
-    //`Can't remove product (id: ${action.id}) as its not in basket!`
-    // );
-    //}
-    //return {
-    // ...state,
-    //basket: newBasket,
-    //};
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket];
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.warn(
+          `Can't remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket,
+      };
     default:
       return state;
   }
