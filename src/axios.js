c6bd4066c6873369fb30ecbeb426837fcdc8dace
taissir-l <taissirlassoued1@gmail.com>
2025-01-25 23:46:43 +0100

    axios changed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import { BASE_URL } from "./constants";
+
 
 const instance = axios.create({
     baseURL: 'http://127.0.0.1:5001/buy-and-go-efb48/us-central1/api' // The API (cloud function) URL
 });
 
 export default instance;
+const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+  
+  export const callAPI = async (resource) => {
+    const { data } = await axios.get(`${BASE_URL}/${resource}`, config);
+    return data;
+  };
